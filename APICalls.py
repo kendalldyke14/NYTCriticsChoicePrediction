1eb88944783dde4cf57610e10e4abdaf22ad271b
kendalldyke14 <kendalldyke14@gmail.com>
2021-12-12 23:18:11 -0500

    adding script to gather data from APIs

---
+++ APICalls.py
@@ -0,0 +1,75 @@
+'''
+This script grabs movie metadata for a current year from New York Times Movie Review API and OMDB API.
+
+DEPENDENCIES: config.py file with New York Times API key and OMDB API key named nyt_key and omdb_key, respectively
+
+OUTPUT: data/NYTData{year}.csv, data/OMDBData{year}.csv
+
+'''
+
+import time
+
+import pandas as pd
+import requests
+import json
+
+from NYTCriticsChoicePrediction import config
+
+
+def get_nyt_movies(key, year):
+    nyt_data = []
+    titles = []
+    has_more = True
+    offset = 0
+    while has_more:
+        # make API request
+        params = {"offset": offset, "publication-date": f"{year}-01-01:{year}-12-31",
+                  "api-key": key}
+        r = requests.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json', params=params)
+
+        #
+        data = json.loads(r.text)
+
+        offset += 20
+        has_more = data['has_more']
+
+
+        nyt_data.extend(data['results'])
+        titles.extend([x['display_title'] for x in data['results']])
+        time.sleep(6)
+
+    nyt_df = pd.DataFrame(nyt_data)
+    nyt_df.to_csv(f"data/NYTData{year}.csv")
+    return nyt_data, titles
+
+
+def get_details_from_omdb(key, year, titles):
+    omdb_data = []
+    for title in titles:
+        omdb_params = {"apikey": key, "t": title, "y": year, "plot": "full"}
+        omdb_baseurl = "http://www.omdbapi.com/"
+        r = requests.get(omdb_baseurl, params=omdb_params)
+        data = json.loads(r.text)
+
+        # if the movies was not found, it may be in the OMDB system under a different year
+        # try taking out the year parameter
+        if json.loads(r.text)["Response"] == "False":
+            omdb_params = {"apikey": key, "t": title, "plot": "full"}
+            r = requests.get(omdb_baseurl, omdb_params)
+            data = json.loads(r.text)
+
+        if data["Response"] == "True":
+            omdb_data.append(data)
+
+    omdb_df = pd.DataFrame(omdb_data)
+    omdb_df.to_csv(f"data/OMDBData{year}.csv")
+    return omdb_data
+
+
+if __name__ == '__main__':
+    nyt_key = config.nyt_key
+    omdb_key = config.omdb_key
+    year = 2020  # Change year here!
+
+    nyt_results, movie_titles = get_nyt_movies(nyt_key, year)
+    omdb_results = get_details_from_omdb(omdb_key, year, movie_titles)
